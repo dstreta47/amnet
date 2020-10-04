f32d975401c059eb1cdd9e1272cd647304fc8493
dstreta47 <diazostretaoffcial@gmail.com>
2020-10-05 00:51:27 +0530

    sixth commit

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ def amnet():
 def aboutme():
     return render_template("aboutme.html")
     
+@app.route("/sample1")
+def sample1():
+    return render_template("sample1.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
